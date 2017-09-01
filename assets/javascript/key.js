f63c705960735d90a9d03a92e9333ba77f0dc875
David Gutierrez <davidgutierrez@Davids-iMac.local>
2017-09-01 09:52:44 -0500

    Further initial setup

---
+++ assets/javascript/key.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'p3NPL3qjb2Z4gzSkTawQI5Jba',
+  consumer_secret: 'hqzdT2MFKwGtAOJrazYgdub187vLwhyS10YXdjf01jwaG5G03g',
+  access_token_key: '903623876348915712-MDRS4RP2aAKc6dpLeIzXfzp5eqnVhCA',
+  access_token_secret: 'IOvN0gK0naRB3C5vVSo5ejBCKOFBXgWiX6GiKvluujn2y',
+}
